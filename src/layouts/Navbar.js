2108fac0b807896fb2662d9e538d4939ae2aeb0b
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-07 08:43:45 +0600

    Add bootstrap

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -3,16 +3,42 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/" className="nav-link">
-        Home
-      </Link>
-      <Link to="/show-books" className="nav-link">
-        Show Books
-      </Link>
-      <Link to="/add-book" className="nav-link">
-        Add Book
-      </Link>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <Link class="navbar-brand" to="/">
+          Redux-Toolkit-Crud-App
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/show-books">
+                Show Books
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/add-book">
+                Pricing
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
